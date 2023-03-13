c6146217d221cf113ae86a3cd97d985fb6ebe070
rbelay1 <ruth_belay@brown.edu>
2023-03-13 16:33:17 -0400

    CaesarCipher
    Debugged and completed Caesar Cipher (1 hour)

--- CaesarCipher.m
+++ CaesarCipher.m
@@ -1,16 +1,20 @@
-function [Cipher]=CaesarCipher(keyword) %function CaesarCipher will take the keyword 
-% and output the enciphered text%
-alphabet=('a':'z'); %vector of the letters of the alphabet%
-keyword='keyword'; %the specific keyword%
-new_alphabet=[]; %an empty vector for the new alphabet for the cipher&
-for ii=1:length(alphabet) %vector of the length of the alphabet%
-    new_alphabet=[keyword,new_alphabet]; %vector of the new alphabet 
-    % that the cipher will use%
-    location=strfind(alphabet,keyword); %finds the indices of the keyword in the alphabet%
-    alphabet(location)=[]; %deletes these indices from the original alphabet%
+function Cipher=CaesarCipher(input,keyword) %function takes in input word and keyword 
+% and outputs ciphered text%
+alphabet='a':'z'; %vector of the alphabet%
+new_alphabet=alphabet; %create a copy of the alphabet under the vector new alphabet%
+Cipher=[]; %create an empty vector for the cipher%
+      for ii=1:length(keyword) %create a vector ii with the length of the keyword%
+          location1=strfind(new_alphabet,keyword(ii)); %find the indices of each letter 
+          % of the keyword in the new alphabet%
+          new_alphabet(location1)=[]; %delete the letters of the keyword 
+          % from the new alphabet%           
+      end 
+new_alphabet=[keyword,new_alphabet]; %create a vector for the new alphabet%
+      for jj=1:length(input) %create a vector jj of the length of the input word%
+          location2=strfind(alphabet,input(jj)); %find the indices of each letter of the 
+          % input word in the original alphabet%
+          output=new_alphabet(location2); %create the output word from the new alphabet%
+          Cipher=[Cipher,output]; %enciphered word%
+      end
+          disp(Cipher);
 end
-Cipher=new_alphabet(keyword); %outputs the enciphered word from the new alphabet%
-disp(Cipher); %displays the enciphered word%
-end
-
-
