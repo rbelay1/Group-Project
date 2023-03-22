8b87338efed6f0ea70c7c58ed9d12db19f6b0063
Aislinn-git <aislinn_baxter@brown.edu>
2023-03-22 12:54:57 -0400

    deciphering and finding column number

--- vigenere_cipher.m
+++ vigenere_cipher.m
@@ -1,7 +1,6 @@
 %% Vigenère cipher %%
 function [ciphertext] = vigenere_cipher(plaintext,keyword);
 %% a:z repeating 26 times but each time indices shift 1
-% rows = (['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']);
 rows = 'a':'z';
 my_mat=[];
 for ii = 0:numel(rows)-1
@@ -9,8 +8,8 @@ tmp = circshift(rows, -ii);
 my_mat = [my_mat;tmp];
 end
 %% keyword repeated until length of plaintext
-keyword = keyword(find(~isspace(keyword))); keyword = lower(keyword);
-plaintext = plaintext(find(~isspace(plaintext))); plaintext = lower(plaintext);
+keyword = keyword(~isspace(keyword)); keyword = lower(keyword);
+plaintext = plaintext(~isspace(plaintext)); plaintext = lower(plaintext);
 keyreps = floor(length(plaintext)/length(keyword));
 extendedkey = [repmat(keyword,1,keyreps) keyword(1:rem(length(plaintext),length(keyword)))];
 %% [key-row, msg-col] to encipher
@@ -21,11 +20,8 @@ for jj = 1:length(plaintext)
     tmp = my_mat(numplain(jj),numkey(jj));%% row A, column L
     ciphertext = [ciphertext tmp];
 end
-disp(ciphertext);
 
 %% [key-row, position of ciphertext-letter in col X to pull column number] to decipher
-
-
 
 end
 
