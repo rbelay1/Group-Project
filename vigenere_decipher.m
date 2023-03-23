124176e05c494f11ff928c3b135653d6b13c77ce
Aislinn-git <aislinn_baxter@brown.edu>
2023-03-22 22:45:08 -0400

    finished decipher

--- vigenere_decipher.m
+++ vigenere_decipher.m
@@ -1,39 +1,26 @@
 %% Vigenere Cipher Deciphering %%
-%% function plaintext = vigenere_decipher(ciphertext,keyword)
+function plaintext = vigenere_decipher(ciphertext,keyword)
 %% a:z block %%
 rows = 'a':'z';
 my_mat=[];
 for ii = 0:numel(rows)-1
 tmp = circshift(rows, -ii);
-my_mat = [my_mat;tmp]
+my_mat = [my_mat;tmp];
 end
 
 %% keyword repeated until length of ciphertext
-ciphertext = 'lxfopvefrnhr'
-keyword = 'lemon';
 keyword = keyword(~isspace(keyword)); keyword = lower(keyword);
 ciphertext = ciphertext(~isspace(ciphertext)); ciphertext = lower(ciphertext);
 keyreps = floor(length(ciphertext)/length(keyword));
-extendedkey = [repmat(keyword,1,keyreps) keyword(1:rem(length(ciphertext),length(keyword)))]
+extendedkey = [repmat(keyword,1,keyreps) keyword(1:rem(length(ciphertext),length(keyword)))];
 
 %% [key-row, position of ciphertext-letter in col X to pull column number] to decipher
 plaintext=[];
 for ii = 1:length(ciphertext)
-    x = double((ciphertext)); numcipher = x-96;   
-    y = double((extendedkey)); numkey = y-96;
-     
-% [c,r] = find(numcipher);
-%% tmp = my_mat(numkey(ii), find(in row == numcipher))
-
-    tmp = my_mat(numkey(ii), numkey(ii)==numcipher);%% row A, column L ??
-
-% A = matrix
-% M = A(:,3:end).';
-% [C,R] = find(M);
-% B = [A(R,1:2),C]
-
-
-plaintext = [plaintext tmp];
+    y = double(extendedkey(ii)); numkey = y-96;
+    col_num = find(my_mat(numkey,:)==ciphertext(ii));
+    letter = char(col_num + 96);
+    plaintext = [plaintext letter];
 end
 disp(plaintext)
 
